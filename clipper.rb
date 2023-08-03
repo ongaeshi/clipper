2ec9c1f2f619b8932693daa999068d5c9d724e34
ongaeshi <ongaeshi0621@gmail.com>
2023-08-04 02:09:54 +0900

    feat: 空白が入っていてもパース可能に

--- clipper.rb
+++ clipper.rb
@@ -1,13 +1,25 @@
 require "clipper"
 require "clipboard"
 
+# Patter1:
+# https://github.com/ongaeshi/clipper ongaeshi/clipper
+# [ongaeshi/clipper](https://github.com/ongaeshi/clipper)
+#
+# Patter2:
+# https://github.com/ongaeshi/clipper ongaeshi/clipper - GitHub
+# [ongaeshi/clipper - GitHub](https://github.com/ongaeshi/clipper)
+def parse_markdown(src)
+  d = src.split(" ")
+  url, title = d[0], d[1..].join(" ")
+  url.strip!
+  title.strip!
+  "[#{title}](#{url})"
+end
+
 def markdown(src)
   lines = src.split("\n")
   text = lines.map do |line|
-    url, title = line.split(" ")
-    url.strip!
-    title.strip!
-    "[#{title}](#{url})"
+    parse_markdown(line)
   end.join("\n")
   Clipboard.copy(text)
 end
