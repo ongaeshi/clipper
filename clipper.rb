e2e61fb2707bd3c7c5944cff633d9b726d2e19f6
ongaeshi <ongaeshi0621@gmail.com>
2023-08-05 01:46:23 +0900

    feat: プラグインをファイル分ける

--- clipper.rb
+++ clipper.rb
@@ -1,64 +1,19 @@
 require "clipper"
 require "clipboard"
-require "uri"
-require "active_support/all"
 
-def uri?(src)
-  URI::DEFAULT_PARSER.make_regexp.match(src)
-end
-
-# Patter1:
-# https://github.com/ongaeshi/clipper ongaeshi/clipper
-# [ongaeshi/clipper](https://github.com/ongaeshi/clipper)
-#
-# Patter2:
-# https://github.com/ongaeshi/clipper ongaeshi/clipper - GitHub
-# [ongaeshi/clipper - GitHub](https://github.com/ongaeshi/clipper)
-#
-# Patter3:
-# ongaeshi/clipper - GitHub https://github.com/ongaeshi/clipper
-# [ongaeshi/clipper - GitHub](https://github.com/ongaeshi/clipper)
-#
-# Patter4:
-#  ongaeshi/clipper - GitHub https://github.com/ongaeshi/clipper 
-# [ongaeshi/clipper - GitHub](https://github.com/ongaeshi/clipper)
-#
-# Patter5:
-# https://github.com/ongaeshi/clipper
-# [https://github.com/ongaeshi/clipper](https://github.com/ongaeshi/clipper)
-def parse_markdown(src)
-  d = src.split(" ")
-  if uri?(d[0])
-    url, title = d[0], d[1..].join(" ")
-  elsif uri?(d[-1])
-    title, url = d[0..-2].join(" "), d[-1]
-  else
-    return src
-  end
-  
-  if title.empty?
-    "[#{url}](#{url})"
-  else
-    "[#{title}](#{url})"
-  end
-end
-
-def markdown(src)
+def eval(src, args)
   lines = src.split("\n")
-  text = lines.compact_blank.map do |line|
-    parse_markdown(line)
+  text = lines.map do |line|
+    line.instance_eval(args[0])
   end.join("\n")
   Clipboard.copy(text)
 end
 
-def eval(src)
-  p "eval #{src}"
-end
-
 def main(argv)
-  command = argv[0]
+  command, args = argv[0], argv[1..]
   src = Clipboard.paste.encode("UTF-8")
-  send(command.to_sym, src)
+  require "clipper/plugin/#{command}"
+  send(command.to_sym, src, args)
 end
 
 main(ARGV)
---
+++ lib/clipper/plugin/markdown.rb
@@ -0,0 +1,52 @@
+require "clipboard"
+require "uri"
+require "active_support/all"
+
+def uri?(src)
+  URI::DEFAULT_PARSER.make_regexp.match(src)
+end
+
+# Patter1:
+# https://github.com/ongaeshi/clipper ongaeshi/clipper
+# [ongaeshi/clipper](https://github.com/ongaeshi/clipper)
+#
+# Patter2:
+# https://github.com/ongaeshi/clipper ongaeshi/clipper - GitHub
+# [ongaeshi/clipper - GitHub](https://github.com/ongaeshi/clipper)
+#
+# Patter3:
+# ongaeshi/clipper - GitHub https://github.com/ongaeshi/clipper
+# [ongaeshi/clipper - GitHub](https://github.com/ongaeshi/clipper)
+#
+# Patter4:
+#  ongaeshi/clipper - GitHub https://github.com/ongaeshi/clipper 
+# [ongaeshi/clipper - GitHub](https://github.com/ongaeshi/clipper)
+#
+# Patter5:
+# https://github.com/ongaeshi/clipper
+# [https://github.com/ongaeshi/clipper](https://github.com/ongaeshi/clipper)
+def parse_markdown(src)
+  d = src.split(" ")
+  if uri?(d[0])
+    url, title = d[0], d[1..].join(" ")
+  elsif uri?(d[-1])
+    title, url = d[0..-2].join(" "), d[-1]
+  else
+    return src
+  end
+
+  if title.empty?
+    "[#{url}](#{url})"
+  else
+    "[#{title}](#{url})"
+  end
+end
+
+def markdown(src, _args)
+  lines = src.split("\n")
+  text = lines.compact_blank.map do |line|
+    parse_markdown(line)
+  end.join("\n")
+  Clipboard.copy(text)
+end
+
