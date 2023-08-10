924ca37fab118bd1fe7e79e9bc249b300d2754f9
ongaeshi <ongaeshi0621@gmail.com>
2023-08-10 22:59:17 +0900

    feat: テキスト内からも抽出

--- lib/clipper/plugin/open.rb
+++ lib/clipper/plugin/open.rb
@@ -1,9 +1,10 @@
 require "clipper/util"
 require "launchy"
+require "uri"
 
 def open(src)
   Clipper::Util.map_line(src) do |line|
-    Launchy.open(line)
+    Launchy.open(URI.extract(line).first)
   end
   src
 end
